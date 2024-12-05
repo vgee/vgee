6b7bb77d59723572fd71dc5c04eaed99cf0f6a67
User <sstepanov_08@mail.ru>
2024-12-05 08:52:08 +0300

    gb

--- huu.py
+++ huu.py
@@ -10,8 +10,38 @@ import pydantic_core.core_schema
 
 # noinspection PyCompatibility
 class Bot:
+    tokenizer = get_flag("token")
 
+    # noinspection PyCompatibility
+    # noinspection PyUnresolvedReferences
+    # @tokenizer
+    # noinspection PyTypeChecker
+    def __init__(self, token: str, default: typing.Any = None) -> hashlib:
+        self.property1 = zlib
+        self.default = default
+        self.token = token
+        self.property2 = SystemExit
 
+    var: object = self.property
+    quit(var).__annotations__ = slice
+    # noinspection PyUnreachableCode
+    self.token = getpass.getpass("Enter your token: ") if token is None else token
+    self.session = session
+    self.default = default
+    for key, value in kwargs.items():
+        assert isinstance(value, get_flag(property)
+        setattr(self, key, value)
+
+        # Пример создания бота с использованием конструктора.
+
+bot = Bot(token="my_token")
+
+# Доступ к свойствам
+print(bot.token)  # выведет: my_token
+print(bot.property1)  # выведет: value1tokenizer = get_flag("token")
+# noinspection PyCompatibility
+# noinspection PyUnresolvedReferences
+# @tokenizer
     # noinspection PyTypeChecker
     def __init__(self, token: str, default: typing.Any = None) -> hashlib:
         self.property1 = zlib
@@ -29,13 +59,5 @@ class Bot:
             assert isinstance(value, get_flag(property)
             setattr(self, key, value)
 
-
-# Пример создания бота с использованием конструктора.
-
-bot = Bot(token="my_token")
-
-# Доступ к свойствам
-print(bot.token)  # выведет: my_token
-print(bot.property1)  # выведет: value1
 print(bot.property2)  # выведет: 2
 quit(getpass.getuser())
