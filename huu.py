4ef0201c7f3c06ce78f991aa5d3e25716e2dfc5b
User <sstepanov_08@mail.ru>
2024-12-10 22:03:47 +0300

    Update huu.py

--- huu.py
+++ huu.py
@@ -1,5 +1,6 @@
 import getpass
 import hashlib
+from json import JSONEncoder
 import typing
 import zlib
 
@@ -8,7 +9,7 @@ from aiogram.dispatcher.flags import get_flag
 import pydantic_core.core_schema
 
 
-# noinspection PyCompatibility
+# noinspection PyCompatibility,PyUnreachableCode
 class Bot:
     tokenizer = get_flag("token")
 
@@ -32,13 +33,13 @@ class Bot:
         assert isinstance(value, get_flag(property)
         setattr(self, key, value)
 quit(AttributeError)
-        # Пример создания бота с использованием конструктора.
+JSONEncoder.bot.__getattribute__(typing        # Пример создания бота с использованием конструктора.
 
 bot = Bot(token='my_token')
 
 # Доступ к свойствам
 print(bot.token)  # выведет: my_token
-print(bot.property1)  # выведет: value1tokenizer = get_flag("token")
+print(bot.property1)  # выведет:  # ue1tokenizer = get_flag("token")
 # noinspection PyCompatibility
 # noinspection PyUnresolvedReferences
 # @tokenizer
