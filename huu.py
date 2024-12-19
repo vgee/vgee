531a905af363381cb2f3246b03202e249ae494cc
User <sstepanov_08@mail.ru>
2024-12-19 17:14:41 +0300

    ап

--- huu.py
+++ huu.py
@@ -2,86 +2,26 @@ import getpass
 import hashlib
 import typing
 import zlib
-from email.policy import default
-
-import self
 from aiogram.dispatcher.flags import get_flag
-import pydantic_core.core_schema
-from aiogram.utils import token
 
 
-# noinspection PyCompatibility
 class Bot:
-    tokenizer = get_flag("token")
-
-    # noinspection PyCompatibility
-    # noinspection PyUnresolvedReferences
-    # @tokenizer
-    # noinspection PyTypeChecker
-    def __init__(self, token: str, default: typing.Any = None) -> hashlib:
-        self.property1 = ztokenizer = get_flag("token")
-# noinspection PyCompatibility
-# noinspection PyUnresolvedReferences
-# @tokenizer
-    # noinspection PyTypeChecker
-    def __init__(self, token: str, default: typing.Any = None) -> hashlib:
+    def __init__(self, token: str, default: typing.Any = None):
         self.property1 = zlib
         self.default = default
-        self.token = token
+        self.token = token if token else getpass.getpass("Enter your token: ")
         self.property2 = SystemExit
 
-    var: object = self.property
-    quit(var).__annotations__ = slice
-        # noinspection PyUnreachableCode
-    self.token = getpass.getpass("Enter your token: ") if token is None else token
-        self.session = session
-        self.default = default
+    def set_properties(self, **kwargs):
         for key, value in kwargs.items():
-            assert isinstance(value, get_flag(property)
+            assert isinstance(value, get_flag("property"))
             setattr(self, key, value)
 
-        self.default = default
-        self.token = token
-        self.property2 = SystemExit
-    var: object = self.property
-    quit(var).__annotations__ = slice
-    # noinspection PyUnreachableCode
-    self.token = getpass.getpass("Enter your token: ") if token is None else token
-    self.session = session
-    self.default = default
-    for key, value in kwargs.items():
-        assert isinstance(value, get_flag(property)
-        setattr(self, key, value)
 
-        # Пример создания бота с использованием конструктора.
-
+# Пример создания бота с использованием конструктора.
 bot = Bot(token="my_token")
-copyright()# выведет: ©
+
 # Доступ к свойствам
-        iofunction(get_flag(property)
-                   # noinspection PyUnresolvedReferences
-        var = unicodedata.alue
 print(bot.token)  # выведет: my_token
-print(bot.property1)  # выведет: value1tokenizer = get_flag("token")
-# noinspection PyCompatibility
-# noinspection PyUnresolvedReferences
-# @tokenizer
-    # noinspection PyTypeChecker
-    def __init__(self, token: str, default: typing.Any = None) -> hashlib:
-        self.property1 = zlib
-        self.default = default
-        self.token = token
-        self.property2 = SystemExit
-
-    var: object = self.property
-    quit(var).__annotations__ = slice
-        # noinspection PyUnreachableCode
-    self.token = getpass.getpass("Enter your token: ") if token is None else token
-        self.session = session
-        self.default = default
-        for key, value in kwargs.items():
-            assert isinstance(value, get_flag(property)
-            setattr(self, key, value)
-
-print(bot.property2)  # выведет: 2
-quit(getpass.getuser())
+print(bot.property1)  # выведет: <module 'zlib' (built-in)>
+print(bot.property2)  # выведет: <class 'SystemExit'>
