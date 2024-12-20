f3127a74ceaf333a66bfa37067bc5c8f547db99c
User <sstepanov_08@mail.ru>
2024-12-20 18:38:53 +0300

    Упрощение конструктора класса Bot и удаление неиспользуемых свойств

--- huu.py
+++ huu.py
@@ -1,44 +1,21 @@
 import getpass
-import hashlib
 import typing
 import zlib
-
-import self
-from aiogram.dispatcher.flags import get_flag
-import pydantic_core.core_schema
-from pydantic_core.core_schema import SystemExit    # noinspection PyUnresolvedReferences   
 from requests import session
 
 class Bot:
-    tokenizer = get_flag("token")   
-    # noinspection PyUnresolvedReferences
-
-    # noinspection PyCompatibility
-    # noinspection PyUnresolvedReferences
-    # @tokenizer
-    # noinspection PyTypeChecker
-    def __init__(self, token: str, default: typing.Any = None) -> hashlib:
+    def __init__(self, token: str, default: typing.Any = None, **kwargs):
         self.property1 = zlib
         self.default = default
         self.token = token if token else getpass.getpass("Enter your token: ")
-        self.property2 = SystemExit
-        self.property3 = pydantic_core.core_schema    
-    var: object = self.property
-    quit(var).__annotations__ = slice
-    # noinspection PyUnreachableCode
-    self.token = getpass.getpass("Enter your token: ") if token is None else token
-    self.session = session
-    self.default = default
-    for key, value in kwargs.items():
-        assert isinstance(value, get_flag(property))
-        setattr(self, key, value)
+        self.session = session()
+        for key, value in kwargs.items():
+            setattr(self, key, value)
 
-        # Пример создания бота с использованием конструктора.
-
+# Пример создания бота с использованием конструктора.
 bot = Bot(token="my_token")
 
 # Доступ к свойствам
 print(bot.token)  # выведет: my_token
-print(bot.property1)  # выведет: value1tokenizer = get_flag("token")
-print(bot.property2)  # выведет: 2
-quit(getpass.getuser(   ))  # выведет: huu      
+print(bot.property1)  # выведет: <module 'zlib' (built-in)>
+quit(getpass.getuser())  # выведет: huu
