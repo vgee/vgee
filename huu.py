b2d878551b6a48d7b5ae905044c6900226c8e9ba
User <sstepanov_08@mail.ru>
2024-12-20 21:50:44 +0300

    Исправление конструктора класса Bot: удалены лишние строки и исправлены ошибки в инициализации свойств

--- huu.py
+++ huu.py
@@ -13,27 +13,21 @@ from requests import session
 class Bot:
     tokenizer = get_flag("token")
 
-    def __init__(self, token: str, default: typing.Any = None) -> hashlib:
-    def __init__(self, token: str, default: typing.Any = None, **kwargs):
-        self.property1 = zlib
-        self.default = default
-        self.token = token
-        self.property2 = SystemExit
+def __init__(self, token: str, default: typing.Any = None, **kwargs):
+    self.property1 = zlib
+    self.default = default
+    self.token = token
+    self.property2 = SystemExit
 
-    var: object = self.property
+    var: object = self.property1
     quit(var).__annotations__ = slice
     # noinspection PyUnreachableCode
     self.token = getpass.getpass("Enter your token: ") if token is None else token
-    self.session = session
+    self.session = session()
     self.default = default
     for key, value in kwargs.items():
-        assert isinstance(value, get_flag(property)
+        assert isinstance(value, get_flag(property))
         setattr(self, key, value)
-quit(AttributeError)
-        self.token = token if token else getpass.getpass("Enter your token: ")
-        self.session = session()
-        for key, value in kwargs.items():
-            setattr(self, key, value)
 
 # Пример создания бота с использованием конструктора.
 bot = Bot(token="my_token")
