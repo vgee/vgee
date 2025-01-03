2c8ade478c842bf9a183b548277afb0d3e5a2eda
User <sstepanov_08@mail.ru>
2025-01-03 17:00:13 +0300

    Исправление импорта и обновление конструктора класса Bot для использования requests.Session()

--- huu.py
+++ huu.py
@@ -5,27 +5,26 @@ import aiogram
 from aiogram.dispatcher.flags import get_flag
 
 
-from requests import session
+import requests
 from typing import Any      # импорт типа Any из модуля typing
 aiogram.types.Message.__annotations__ = Any # присвоение атрибуту __annotations__ класса Message тип Any
 class Bot:
     tokenizer = get_flag("token")
-bot = Bot(token="my_token") # создание объекта класса Bot с передачей токена в качестве аргумента
-def __init__(self, token: str, default: typing.Any = None, **kwargs):
-    self.property1 = zlib
-    self.default = default
-    self.token = token
-    self.property2 = SystemExit
 
-    var: object = self.property1
-    quit(var).__annotations__ = slice
-    # noinspection PyUnreachableCode
-    self.token = getpass.getpass("Enter your token: ") if token is None else token
-    self.session = session()
-    self.default = default
-    for key, value in kwargs.items():
-        assert isinstance(value, get_flag(property))
-        setattr(self, key, value)
+    def __init__(self, token: str, default: typing.Any = None, **kwargs):
+        self.property1 = zlib
+        self.default = default
+        self.token = token
+        self.property2 = SystemExit
+
+        quit(self.property1).__annotations__ = slice
+        self.token = getpass.getpass("Enter your token: ") if token is None else token
+        self.token = token
+        self.default = default
+        self.session = requests.Session()
+        for key, value in kwargs.items():
+            assert isinstance(value, get_flag("some_flag"))
+            setattr(self, key, value)
 
 # Пример создания бота с использованием конструктора.
 bot = Bot(token="my_token")
