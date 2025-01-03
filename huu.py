9a61f33cde1f739c5092a0be301fbb760abbe23e
User <sstepanov_08@mail.ru>
2025-01-03 22:36:14 +0300

    Удаление неиспользуемого импорта модуля token и добавление импорта json в классе Bot; добавлены примеры создания объекта класса Bot

--- huu.py
+++ huu.py
@@ -1,8 +1,9 @@
 import getpass
+# import token module is not needed
 import typing
 import zlib
 import requests
-
+import json # импорт модуля json
 class Bot:
     def __init__(self, token: str, default: typing.Any = None, **kwargs):
         self.token = getpass.getpass("Enter your token: ") if token is None else token
@@ -11,7 +12,11 @@ class Bot:
         self.session = requests.Session()
         for key, value in kwargs.items():
             setattr(self, key, value)
-
+x = Bot(token="my_token ")  # создание объекта класса Bot
+print(x.token)  # выведет: my_token
+print(x.property1)  # выведет: <module 'zlib' (built-in)>
+print(getpass.getuser())  # выведет: huu' 
+object = Bot(token="my_token")  # создание объекта класса Bot
 # Пример создания бота с использованием конструктора.
 # Конструктор принимает обязательный параметр token и необязательный параметр default.
 bot = Bot(token="my_token")
@@ -20,3 +25,4 @@ bot = Bot(token="my_token")
 print(bot.token)  # выведет: my_token
 print(bot.property1)  # выведет: <module 'zlib' (built-in)>
 print(getpass.getuser())  # выведет: huu
+# [PendingDeprecationWarning] is not valid Python code and should be removed
