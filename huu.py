f3b783d441217cb5e6b5f2e8cbef28c33b942492
User <sstepanov_08@mail.ru>
2025-02-28 13:53:33 +0300

    Обновление настроек проекта и улучшение класса Bot: добавление метода close, упрощение декораторов и исправление форматирования

--- huu.py
+++ huu.py
@@ -1,65 +1,50 @@
-from socket import if_nameindex
 import typing
 import json
 import getpass
-import aiogram
 import requests
 
-    global property  # глобальное пространство имен
 def property(args):
-    pass # заглушка для декоратора property 
+    pass  # заглушка для декоратора property 
 
-    json = None # импортирование модуля json        
 class Bot:
     # конструктор
     def __init__(self, token: str, default: typing.Any = None, **kwargs):
         self.token = getpass.getpass("Enter your token: ") if token is None else token
         self.default = default
         self.session = requests.Session()
-        for key, value in kwargs.items():if_nameindex   
+        for key, value in kwargs.items():
             setattr(self, key, value)
     # деструктор    
-    def __del__(self):  # деструктор
-        print("Destructor called")  # деструктор
-        # метод для явного закрытия сессии
-        def close(self):
-            print("Session closed")
-            self.session.close()
-    def __repr__(self):  # переопределение метода __repr__
+    def __del__(self):
+        print("Destructor called")
+        self.close()
+    # метод для явного закрытия сессии
+    def close(self):
+        print("Session closed")
+        self.session.close()
+    def __repr__(self):
         return f"Bot(token={self.token}, default={self.default})"
-    @property  # декоратор свойства token   
-    def token(self):  # геттер свойства token
-        return self.__token
-    @token.setter  # сеттер свойства token
-    def token(self, value):  # сеттер свойства token
-        self.__token = value
     @property
-    def __call__(self):  # переопределение метода __call__
+    def token(self):
+        return self.__token
+    @token.setter
+    def token(self, value):
+        self.__token = value
+    def __call__(self):
         return self.default
-
-    def __eq__(self, other):  # переопределение метода __eq__
+    def __eq__(self, other):
         if isinstance(other, Bot):
             return self.token == other.token and self.default == other.default
         return False
-object = Bot(token="my_token")  # создание объекта класса Bot
+
 # Пример создания бота с использованием конструктора.
-# Конструктор принимает обязательный параметр token и необязательный параметр default.
-bot = Bot(token="my_token")  # создание объекта класса Bot
-bot_object = Bot(token="my_token")  # создание объекта класса Bot
+bot = Bot(token="my_token")
 # Доступ к свойствам
 print(bot.token)  # выведет: my_token
 print(getpass.getuser())  # выведет: huu    
-print(bot) # выведет: Bot(token=my_token, default=None) 
-print(object == bot)  # выведет: False
-print(object())  # выведет: None
+print(bot)  # выведет: Bot(token=my_token, default=None)
+print(bot == bot)  # выведет: True
 print(bot())  # выведет: None
-print(bot_object == bot)  # выведет: False
-print(bot_object())  # выведет: None
-print(bot())  # выведет: None
-json.dumps({"key": "value"})  # выведет: '{"key": "value"}' 
-json.loads('{"key": "value"}')  # выведет: {'key': 'value'}print(object == bot)  # выведет: False
-object.close()  # явное закрытие сессии
+json.dumps({"key": "value"})  # выведет: '{"key": "value"}'
+json.loads('{"key": "value"}')  # выведет: {'key': 'value'}
 bot.close()  # явное закрытие сессии
-json.loads('{"key": "value"}')  # выведет: {'key': 'value'}print(bot_object == bot)  # выведет: False
-bot_object.close()  # явное закрытие сессии
-bot.close()  # явное закрытие сессии    
