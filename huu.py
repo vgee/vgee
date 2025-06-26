2da9b4e69bcd689de3bd55870b433596b2066f87
User <sstepanov_08@mail.ru>
2025-06-26 06:33:24 +0300

    Удалены неиспользуемые методы и переопределения в классе Bot. Упрощен код.

--- huu.py
+++ huu.py
@@ -17,29 +17,7 @@ class Bot:
         print("Session closed")
         self.session.close()
 aiogram.Bot = Bot  # переопределение aiogram.Bot для использования нашего класса    
-    def __enter__(self):
-        return self
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        self.close()
-    def __repr__(self):
-        return f"Bot(token={self.token}, default={self.default})"
-    @property
-    @token.setter
-    def token(self, value: str) -> None:
-        self.__token = value
-    def token(self, value):
-        self.__token = value
-    def __call__(self):
-        return self.default
-    def __eq__(self, other):
-        if isinstance(other, Bot):
-            return self.token == other.token and self.default == other.default
-        return False
-# Пример создания бота с использованием конструктора.
-bot = Bot(token="my_token")
-# Доступ к свойствам
-print(bot.token)  # выведет: my_token
+ print(bot.token)  # выведет: my_token
 print(getpass.getuser())  # выведет: huu    
 print(bot)  # выведет: Bot(token=my_token, default=None)
 print(bot == bot)  # выведет: True
