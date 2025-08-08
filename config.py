7307ee877580b879bb0c61dbd056131e790eee2f
User <sstepanov_08@mail.ru>
2025-08-08 10:13:07 +0300

    Changes

--- config.py
+++ config.py
@@ -3,3 +3,14 @@ import os
 class Config:
     TOKEN = os.getenv("BOT_TOKEN", "your_default_token_here")
     DEFAULT_SETTING = os.getenv("DEFAULT_SETTING", "default_value")
+    PREFIX = os.getenv("PREFIX", "!")
+    def __init__(self):
+        if not self.TOKEN:
+            raise ValueError("BOT_TOKEN is not set in the environment variables")
+        else:
+            print("Bot token is set")
+            if not self.PREFIX:
+                if self.DEFAULT_SETTING:
+                    print("Prefix is set to default value")
+                else:
+                    raise ValueError("PREFIX is not set in the environment variables")
