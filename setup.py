c9dbc5f14a7a6e1a11b5d28135358df6330728d6
Сергей Степанов <sstepanov_08@live.ru>
2025-12-08 21:37:24 +0300

    Добавлен файл setup.py для настройки пакета vgee

---
+++ setup.py
@@ -0,0 +1,13 @@
+# python
+from setuptools import setup, find_packages
+
+setup(
+    name='vgee',
+    version='0.1.0',
+    packages=find_packages(),
+    url='',
+    license='',
+    author='User',
+    author_email='',
+    description=''
+)
